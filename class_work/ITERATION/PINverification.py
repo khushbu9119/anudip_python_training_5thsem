3787d679f605db34a71daaa39cb6c9191b175362
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-04 11:47:07 +0530

    program to PIN Verification

--- class_work/ITERATION/PINverification.py
+++ class_work/ITERATION/PINverification.py
@@ -1,6 +1,9 @@
+#********** PIN Verification Program *********
 pin = int(input("Enter your 4-digit PIN: "))
 correct_pin = 1234
+# *************Verify the PIN************
 while (pin != correct_pin):
     print("Incorrect PIN. Please try again.")
     pin = int(input("Enter your 4-digit PIN: "))
+#**********PIN verified successfully. Access granted.************
 print("PIN verified successfully. Access granted.")
