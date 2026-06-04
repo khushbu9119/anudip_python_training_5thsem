c5403aad82d41802b56d901596f35c7398db2125
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-04 15:56:02 +0530

    Pattern-Based Number Pyramid

--- lab_work/iteration_4th_june/PatternBased.py
+++ lab_work/iteration_4th_june/PatternBased.py
@@ -1,7 +1,15 @@
-# Pattern Based Number Printing
+# Program to print forward and reverse pattern
+
 rows = int(input("Enter number of rows: "))
-# Print the pattern
-for i in range(1, rows + 1):    # Loop for each row
-    for j in range(1, i + 1):    # Loop for printing numbers in each row
-        print(j, end="")        # Print numbers without newline
-    print()                     # Move to the next line after each row
+
+# Forward Pattern
+for i in range(1, rows + 1):         # Loop for each row
+    for j in range(1, i + 1):           # Loop for printing numbers in each row
+        print(j, end="")                   # Print numbers without newline
+    print()                              # Move to the next line after each row
+
+# Reverse Pattern
+for i in range(rows, 0, -1):        # Loop for each row in reverse order
+    for j in range(1, i + 1):           # Loop for printing numbers in each row
+        print(j, end="")            # Print numbers without newline
+    print()                        # Move to the next line after each row
