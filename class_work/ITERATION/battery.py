92019b084df25afc5538695420cb2a5cef321fd0
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-04 11:02:20 +0530

    program to count student

--- class_work/ITERATION/battery.py
+++ class_work/ITERATION/battery.py
@@ -1,5 +1,9 @@
 charging_level=20
+
 while(charging_level<=100):
-    print("battery is charging",charging_level,"%")
-    charging_level=charging_level + 10
+   
+        print("battery is charging",charging_level,"%")
+        charging_level=charging_level + 12
+   
+
 print("full charged")
