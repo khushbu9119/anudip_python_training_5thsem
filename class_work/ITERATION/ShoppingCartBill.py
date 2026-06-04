be23d40452215bca0de144b557392f32783688a9
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-04 11:57:59 +0530

    program to Shopping Cart Bill

--- class_work/ITERATION/ShoppingCartBill.py
+++ class_work/ITERATION/ShoppingCartBill.py
@@ -1,8 +1,10 @@
+#*********Enter the no of itenms***********88
 no_of_item = int(input("Enter the number of items you want to purchase: "))
 total_bill = 0
+#*********Calculate the total bill************
 while (no_of_item > 0):
     price = float(input("Enter the price of item: "))
     total_bill += price
     no_of_item -= 1
-
+#*********Display the total bill************
 print("Total bill: $", total_bill)
