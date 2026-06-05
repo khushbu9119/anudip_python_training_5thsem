cbd32abd16b52c4902d39d935537003a494993b3
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-05 14:44:14 +0530

    Student Performance Analyzer

---
+++ class_work/list_5th_june/StudentPerformanceAnalyzer.py
@@ -0,0 +1,43 @@
+
+# List of student marks
+marks = [78, 45, 92, 35, 88, 40, 99, 56]
+
+# 1. Display all passed students (marks >= 40)
+print("Passed Students Marks:")
+for i in marks:
+    if i >= 40:
+        print(i)
+
+# 2. Count the number of failed students
+fail_count = 0
+
+for i in marks:
+    if i < 40:
+        fail_count += 1
+
+print("Number of Failed Students:", fail_count)
+
+# 3. Find highest and lowest marks without using max() or min()
+
+highest = marks[0]
+lowest = marks[0]
+
+for i in marks:
+    if i > highest:
+        highest = i
+
+    if i < lowest:
+        lowest = i
+
+print("Highest Marks:", highest)
+print("Lowest Marks:", lowest)
+
+# 4. Create a new list containing marks above 75
+
+above_75 = []
+
+for i in marks:
+    if i > 75:
+        above_75.append(i)
+
+print("Marks Above 75:", above_75)
