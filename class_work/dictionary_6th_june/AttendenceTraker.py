49f2ec1929a27f15351fdf43c5184c3b931399fa
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-06 15:39:29 +0530

    Attendence Traker

---
+++ class_work/dictionary_6th_june/AttendenceTraker.py
@@ -0,0 +1,23 @@
+# Attendance Tracker Using Dictionary
+
+attendance = {}
+
+# Input attendance of 30 students
+
+for i in range(30):
+
+    roll = int(input("Enter Roll Number : "))
+    status = input("Enter Attendance (Present/Absent) : ")
+
+    attendance[roll] = status
+
+print("----------------------")
+
+# Display roll numbers of present students
+
+print("Students who are Present")
+
+for roll in attendance:
+
+    if attendance[roll] == "Present":
+        print("Roll Number :", roll)
