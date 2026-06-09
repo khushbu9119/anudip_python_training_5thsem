08ede234c408c232f3668e491b4afb4414ce0a60
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-09 14:37:40 +0530

    Intrest Calculator

--- class_work/function_9th_june/IntrestCalculate.py
+++
@@ -1,9 +0,0 @@
-# Simple interest calculation
-def simple_interest(principal, rate, time):
-    interest = (principal * rate * time) / 100
-    return interest
-# Compound interest calculation
-def compound_interest(principal, rate, time):
-    amount = principal * (1 + rate / 100) ** time
-    interest = amount - principal
-    return interest
