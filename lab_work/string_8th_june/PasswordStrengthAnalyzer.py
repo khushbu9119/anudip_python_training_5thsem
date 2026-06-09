f93ece086f19c2acf4025f26cfec7689356814ae
khushbu9119 <khushbuchaudhary9119@gmail.com>
2026-06-09 13:49:20 +0530

    Text Compression Analyzer

--- lab_work/string_8th_june/PasswordStrengthAnalyzer.py
+++ lab_work/string_8th_june/PasswordStrengthAnalyzer.py
@@ -1,5 +1,7 @@
+# Input password
 password = "Python@2026!"
 
+# Variables
 upper = 0
 lower = 0
 digits = 0
@@ -8,28 +10,38 @@ special = 0
 digit_list = []
 special_list = []
 
+# Check every character
 for ch in password:
+
+    # Count uppercase letters
     if ch.isupper():
         upper += 1
 
+    # Count lowercase letters
     elif ch.islower():
         lower += 1
 
+    # Count digits
     elif ch.isdigit():
         digits += 1
         digit_list.append(ch)
 
+    # Count special characters
     else:
         special += 1
         special_list.append(ch)
 
+# Check password strength
 if len(password) >= 8 and upper >= 1 and lower >= 1 and digits >= 1 and special >= 1:
     strength = "Strong"
+
 elif len(password) >= 6:
     strength = "Medium"
+
 else:
     strength = "Weak"
 
+# Display result
 print("Password:", password)
 print("Uppercase Letters:", upper)
 print("Lowercase Letters:", lower)
